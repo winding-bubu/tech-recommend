f89bf8d4a34b435e76f768366f6ededfca05a1d5
hwinte <15764220494@163.com>
2025-06-22 14:06:32 +0800

    构建完整示例

--- tech-recommend-common/src/main/java/com/tech/recommend/common/constant/DslBuildEnum.java
+++ tech-recommend-common/src/main/java/com/tech/recommend/common/constant/DslBuildEnum.java
@@ -11,7 +11,7 @@ import lombok.Getter;
 @Getter
 public enum DslBuildEnum {
 
-    FROWARD("forward", "正向遍历"),
+    FROWARD("forward", "正向构建dsl，通过dsl模板拼接参数生成"),
 
     BACKWARD("backward", "反向组合"),
 
